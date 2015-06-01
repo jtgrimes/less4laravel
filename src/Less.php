a2ad2f31ca623f697cbdcef879ca6567c4e0d97f
Pavel Belousov <pafnuty10@gmail.com>
2015-06-02 01:21:36 +0300

    Update Less.php

--- src/Less.php
+++ src/Less.php
@@ -1,52 +1,57 @@
 <?php namespace Jtgrimes\Less4laravel;
 
-use lessc;
-use Illuminate\Contracts\Config\Repository as Config;
 use Collective\Html\HtmlBuilder as Html;
+use Illuminate\Contracts\Config\Repository as Config;
+use lessc;
 
 class Less {
 	var $config;
 	var $builder;
 
 	public function __construct(Config $config, Html $builder) {
-		$this->config = $config;
+		$this->config  = $config;
 		$this->builder = $builder;
 	}
 
-	public function to($filename, $attributes=array()) {
+	public function to($filename, $attributes = array()) {
+		$link = $this->link($filename, $attributes = array());
+		return $this->builder->style($link, $attributes);
+	}
+
+	public function link($filename, $attributes = array()) {
 		$compiler = new lessc;
-        $compiler->setFormatter($this->config->get('less4laravel.formatter','lessjs'));
-		$basePath = base_path();
+		$compiler->setFormatter($this->config->get('less4laravel.formatter', 'lessjs'));
+		$basePath     = base_path();
 		$sourceFolder = $this->config->get('less4laravel.source_folder');
 		$targetFolder = $this->config->get('less4laravel.target_folder');
-		$caheFolder = $this->config->get('less4laravel.cahce_folder');
-		$in = "$basePath/$sourceFolder/$filename.less";
-		$out = "$basePath/$targetFolder/$filename.css";
-		$cache = "$basePath/$caheFolder/$filename.less.cache";
-		switch($this->config->get('less4laravel.compile_frequency')) {
+		$caheFolder   = $this->config->get('less4laravel.cahce_folder');
+		$in           = "$basePath/$sourceFolder/$filename.less";
+		$out          = "$basePath/$targetFolder/$filename.css";
+		$cache        = "$basePath/$caheFolder/$filename.less.cache";
+		switch ($this->config->get('less4laravel.compile_frequency')) {
 			case "all":
 				$compiler->compileFile($in, $out);
 				break;
 			case "changed":
-                $compiler->checkedCompile($in, $out);
+				$compiler->checkedCompile($in, $out);
 				break;
 			case "cached":
-                if (file_exists($cache)) {
-                    $cacheData = unserialize(file_get_contents($cache));
-                } else {
-                    $cacheData = $in;
-                }
-                $newCache = $compiler->cachedCompile($cacheData);
-                if (!is_array($cacheData) || $newCache["updated"] > $cacheData["updated"]) {
-                    file_put_contents($cache, serialize($newCache));
-                    file_put_contents($out, $newCache['compiled']);
-                }
-                break;
+				if (file_exists($cache)) {
+					$cacheData = unserialize(file_get_contents($cache));
+				} else {
+					$cacheData = $in;
+				}
+				$newCache = $compiler->cachedCompile($cacheData);
+				if (!is_array($cacheData) || $newCache["updated"] > $cacheData["updated"]) {
+					file_put_contents($cache, serialize($newCache));
+					file_put_contents($out, $newCache['compiled']);
+				}
+				break;
 			case "none":
 			default:
 				// do nothing
 		}
 		$linkFolder = $this->config->get('less4laravel.link_folder');
-		return $this->builder->style("$linkFolder/$filename.css",$attributes);
+		return $linkFolder . '/' . $filename . '.css';
 	}
 }
